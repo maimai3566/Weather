47a54e253cb7742c3bbc0643d2177c794faf30c4
user <ru2021kko@gmail.com>
2025-04-10 06:34:40 +0900

    画面作った

--- app/src/main/java/com/rururi/weather/MainActivity.kt
+++ app/src/main/java/com/rururi/weather/MainActivity.kt
@@ -1,24 +1,32 @@
 package com.rururi.weather
 
+import android.R.attr.onClick
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.rururi.weather.ui.theme.WeatherTheme
 import kotlinx.coroutines.sync.Mutex
 
@@ -41,7 +49,27 @@ fun WeatherApp() {
 
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) { //引数にmodifierを設定
-    
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.tenki0),
+            contentDescription = null,
+            modifier = Modifier
+                .padding(top = 120.dp)
+                .size(360.dp)
+                .aspectRatio(1f)
+        )
+        Button(
+            onClick = {},
+            modifier= Modifier
+                .padding(top = 100.dp)
+                .size(width = 200.dp, height = 64.dp)
+        ){
+            Text(text="明日の天気は？",fontSize = 20.sp)
+        }
+    }
 }
 
 @Preview(showBackground = true) //プレビュー画面に表示
