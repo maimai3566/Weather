a32287acf3334208861c61542e0e1a06f43c0feb
user <ru2021kko@gmail.com>
2025-04-10 20:33:11 +0900

    一旦ここまで

--- app/src/main/java/com/rururi/weather/MainActivity.kt
+++ app/src/main/java/com/rururi/weather/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.rururi.weather
 
 import android.R.attr.onClick
+import android.R.attr.visible
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -20,6 +27,12 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -28,7 +41,10 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.rururi.weather.ui.theme.WeatherTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.sync.Mutex
+import java.lang.System.exit
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,20 +65,46 @@ fun WeatherApp() {
 
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) { //引数にmodifierを設定
+    //画像を表示するリストを作成
+    val images = listOf(
+        R.drawable.tenki1,
+        R.drawable.tenki2,
+        R.drawable.tenki3,
+        R.drawable.tenki4,
+        R.drawable.tenki5,
+        R.drawable.tenki6,
+    )
+    var randomImage by remember { mutableStateOf(R.drawable.tenki0) } //画像リストからランダムに画像を表示
+    var isShowImage by remember { mutableStateOf(true) }
+    val scope = rememberCoroutineScope()
+
     Column(
         modifier = modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ){
-        Image(
-            painter = painterResource(id = R.drawable.tenki0),
-            contentDescription = null,
-            modifier = Modifier
-                .padding(top = 120.dp)
-                .size(360.dp)
-                .aspectRatio(1f)
-        )
+        AnimatedVisibility(
+            visible = isShowImage,
+            enter = fadeIn(),
+            exit = fadeOut()
+        ) {
+            Image(
+                painter = painterResource(id = randomImage),
+                contentDescription = null,
+                modifier = Modifier
+                    .padding(top = 120.dp)
+                    .size(360.dp)
+                    .aspectRatio(1f)
+            )
+        }
         Button(
-            onClick = {},
+            onClick = {
+                isShowImage = false //画像をいったん非表示に
+                scope.launch {
+                    delay(300)
+                    randomImage = images.random()
+                    isShowImage = true //画像を表示
+                }
+            },
             modifier= Modifier
                 .padding(top = 100.dp)
                 .size(width = 200.dp, height = 64.dp)
